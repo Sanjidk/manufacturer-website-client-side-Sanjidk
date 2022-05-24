dedea8a29e0bdc3e7c1e701b5486d0a6fc4577f0
Mahmud Sanjid <sanjidk24@gmail.com>
2022-05-24 16:56:43 +0600

    login signup done

--- src/App.js
+++ src/App.js
@@ -11,29 +11,35 @@ import ToolDetails from './components/Home/ToolDetails/ToolDetails';
 
 import Login from './components/Login/Login/Login';
 import Signup from './components/Login/Signup/Signup';
+import RequireAuth from './components/Login/RequireAuth/RequireAuth';
 import NotFound from './components/Shared/NotFound/NotFound';
-
+import Dashboard from './components/Home/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto px-5">
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/tools" element={<Tools></Tools>}></Route>
-        <Route path="/business" element={<BusinessSummary></BusinessSummary>}></Route>
-        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
-        <Route path="/portfolio" element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/business" element={<BusinessSummary />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/portfolio" element={<MyPortfolio />} />
 
-        <Route path="/items/:id" element={<ToolDetails></ToolDetails>}></Route>
+        <Route path="dashboard" element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>} >
+        </Route>
 
+        <Route path="/tool/:id" element={<ToolDetails></ToolDetails>}></Route>
 
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
-      
+
     </div>
   );
 }
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,7 +1,26 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../Loading/Loading';
 
 const Header = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        <p className='text-red-500'><small>{error?.message}</small></p>
+    }
+
+    const logout = () => {
+        signOut(auth);
+        localStorage.removeItem('accessToken');
+    };
 
     const mainMenu = <>
         <li><Link to="/home">Home</Link></li>
@@ -9,15 +28,20 @@ const Header = () => {
         <li><Link to="/business">Business Summary</Link></li>
         <li><Link to="/blogs">Blogs</Link></li>
         <li><Link to="/portfolio">My Portfolio</Link></li>
-        
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
+        {
+            user && <li><Link to="/dashboard">Dashboard</Link></li>
+        }
+        <li>{
+        user ? <button onClick={logout} >Logout</button> 
+        :
+         <Link to="/login">Login</Link>
+         }</li>
+    
 
-        {/* <li><Link to="/dashboard">Dashboard</Link></li> */}
     </>
 
     return (
-        <div className="navbar bg-sky-50 rounded mt-2 sticky top-0">
+        <div className="navbar bg-sky-50 rounded sticky top-0">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
