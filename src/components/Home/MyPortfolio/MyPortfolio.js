55f4aef7970c631d2f865fda1caa55971430e9dd
Mahmud Sanjid <sanjidk24@gmail.com>
2022-05-28 04:50:40 +0600

    Edit Portfolio

--- src/components/Home/MyPortfolio/MyPortfolio.js
+++ src/components/Home/MyPortfolio/MyPortfolio.js
@@ -8,8 +8,16 @@ const MyPortfolio = () => {
                 <img src={sanjid} alt="" />
                 <div>
                     <h1 className="text-5xl font-bold">Md. Mahmud Sanjid</h1>
-                    <p className="text-2xl font-bold py-6">MERN Developer </p>
-                    <button className='btn btn-primary'><a href="https://drive.google.com/file/d/1bk7GAU-l5VU-Jz3RrBHoPAa_YTS960At/view?usp=sharing" target='_blank'>My Resume</a></button>
+                    <p className="text-2xl font-bold"> MERN Developer </p>
+                    <p className="text-xl">Email: sanjidk24@gmail.com </p>
+                    <p className="text-xl mb-3">MSc (Running) : Jahangirnagar University</p>
+                    <p className="font-bold mb-3">SKILLS and TECHNOLOGIES
+                        <li>Expertise: JavaScript (ES6), React.js, Bootstrap5, Tailwind, Daisy UI</li>
+                        <li>Comfortable: Express JS, Node JS, MongoDB</li>
+                        <li>Design Skills: Adobe Photoshop, Figma</li>
+                        <li>Tools and Software's: Git, VS Code, Chrome dev tools, Heroku, Netlify, Firebase.</li>
+                         </p>
+                    <button className='btn btn-primary'><a href="https://drive.google.com/file/d/1bk7GAU-l5VU-Jz3RrBHoPAa_YTS960At/view?usp=sharing" target='_blank'>See Resume for Project Link</a></button>
                     
                 </div>
             </div>
