f71c21a20f03da12dbebdffb77f07a64b12b6ce6
Mahmud Sanjid <sanjidk24@gmail.com>
2022-05-27 16:20:25 +0600

    edit readme file and blogs page

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# Getting Started with Create React App
+# This is a React Project
 
-This project was bootstrapped with [Create React App](https://manufacture-tool.web.app/).
+This project is about DSLR Manufacture Tools [Live Link](https://manufacture-tool.web.app/).
+
+
+## About this Project
+
+**** Before User Login ****
+
+* In this project, in home page, there has some information like Carousel, Blogs,Tools, Business Summary etc.
+* In Blogs, there have 6 answer of question.
+* In Items, there has shown 6 items including name, price, available quantity,Minimum quantity, short description and `Buy Now` button.
+* After clicking `Buy Now` Button, if you are login this website,, you can see details and more fields for purchase. But if you are not log in, it will redirect login page for login or signUp.
+* In Business Summary page, you can see some static summary.
+
+
+**** After User Login ****
+
+* For login, you have to signUp by name, gmail and password. you can also login by google. In Header, there has two button login & signUp. 
+* After login by user, user can see `Tools Page`, `Dashboard`etc in Header section.
+*  In Dashboard section, there has some nested route like `All User`, `My Orders`, `My Reviews`, `My Profile` etc.
+*  If user get role as Admin, then he can see `All Users` and he can make admin to others. Otherwise no user can see `All Users` route.
+*  In `My Orders` route, user can see their purchase order product.
+*  In `My Reviews` route, user can add reviews and publish their reviews.
+*  In `My Profile` route, user can see and update their profile.
+
+** That is the summary of Project. **
+
+
+## List of Technologies.
+
+* React JS
+* Node JS
+* React Router
+* React Query
+* Heroku Deploy
+* React firebase hooks
+* Mongodb Atlas
+* Firebase
+* Tailwind CSS
+* Daisy UI
+* Toastify
--- src/components/Home/Blogs/Blogs.js
+++ src/components/Home/Blogs/Blogs.js
@@ -7,46 +7,59 @@ const Blogs = () => {
             <div className="collapse rounded-lg mt-2">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    Click me to show/hide content
+                    How will you improve the performance of a React Application ?
                 </div>
                 <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+                    <p>
+                        <li>Keeping component state local where necessary.</li>
+                        <li>Windowing or list virtualization in React.</li>
+                        <li>Memoizing React components to prevent unnecessary re-renders.</li>
+                        <li>Lazy loading images in React.</li>
+                        <li>Code-splitting in React using dynamic import()</li>
+                    </p>
                 </div>
             </div>
             <div className="collapse rounded-lg mt-2">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    Click me to show/hide content
+                    What are the different ways to manage a state in a React application ?
                 </div>
                 <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+                    <p>5 Types of Application State in React and How They Help in State Management</p>
+                    <p>
+                        <li>Communication State: Communication state forms the backbone of your React Native app without you even knowing about it. Remember when you introduced the communication system in your app.</li>
+                        <li>Data State: Data state covers information that your React application stores temporarily for various business functions. Supposedly, you are building a project management app.</li>
+                        <li>Control State: Contrary to the state mentioned above in a React app, the control state does not represent the applications environment. Instead, it refers to the state which the user has input into the app.</li>
+                        <li>Session State: Session state contains information about the user of the application such as user id, permissions, passwords, etc.</li>
+                        <li>Location State: Location state is the UTF-8 display that appears in your URL bar. Also, the HTML5 History API allows you to store states separately from the specific URL.</li>
+                    </p>
                 </div>
             </div>
             <div className="collapse rounded-lg mt-2">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    Click me to show/hide content
+                    How does prototypical inheritance work ?
                 </div>
                 <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+                    <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
                 </div>
             </div>
             <div className="collapse rounded-lg mt-2">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    Click me to show/hide content
+                    What is a unit test? Why should write unit tests ?
                 </div>
                 <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+                    <p>Unit test: Unit testing is a software development process in which the smallest testable parts of an application, called units, are individually and independently scrutinized for proper operation.</p>
+                    <p>One of the benefits of unit tests is that they isolate a function, class or method and only test that piece of code. Higher quality individual components create overall system resiliency. Thus, the result is reliable code. Unit tests also change the nature of the debugging process.</p>
                 </div>
             </div>
             <div className="collapse rounded-lg mt-2">
                 <input type="checkbox" className="peer" />
                 <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    Click me to show/hide content
-                </div>
+                    Why you do not set the state directly in React ?                </div>
                 <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-                    <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+                    <p>Because, One should never update the state directly because of the following reasons: If you update it directly, calling the setState() afterward may just replace the update you made. When you directly update the state, it does not change this.</p>
                 </div>
             </div>
             <Footer></Footer>
